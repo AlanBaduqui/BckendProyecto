3a75a4b88953363d80b319ab4b735135034529a0
Alan Alexis Baduqui Bueno <61479940+AlanBaduqui@users.noreply.github.com>
2021-05-31 10:42:56 -0700

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = process.env.MONGODB_URI
   ? process.env.MONGODB_URI
-  : 'mongodb://localhost/reportes';
+  : 'mongodb+srv://Alan:alan.011292@cluster0.pmimh.mongodb.net/myFirstDB?retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
